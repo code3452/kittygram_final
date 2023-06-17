f2eddcfbf65655c35bc6fc4a8090ec4e88a00065
Дмитрий Прибавкин <Pribavkin.dmitrij@yandex.ru>
2023-06-17 18:12:55 +0500

    mistakes fixed

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,6 +10,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['kitthlm.ddns.net', 'localhost', '127.0.0.1']
